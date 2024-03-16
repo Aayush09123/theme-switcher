091334eab5cf4bb58c032a89407dfb91aae13acc
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-03-16 15:26:33 +0530

    create a context

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark")
+    document.querySelector("html").classList.add(themeMode)
+  }, [themeMode])
 
   return (
-    <>
-      <h1 className='bg-blue-800 text-white text-3xl p-4'>Theme Switcher</h1>
-    </>
+
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
